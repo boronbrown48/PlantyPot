3f302308d45c32edbe4f9d1707173c3a8c3e06bc
Filmptz <you@example.com>
2021-05-22 14:59:25 +0700

    Query By plant name

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
-import requests 
+import requests ,json 
 import microgear.client as microgear
 import time
 import logging
+
+#Global variable
+result = json.dumps(None)
 
 #connect with line notify
 
@@ -24,6 +27,9 @@ def connection():
     logging.info("Now I am connected with netpie")
 def subscription(topic,message):
     logging.info(topic+" "+message)
+    global result
+    msg = message.split("'")
+    result = SearchData(msg[1])
 def disconnect():
     logging.debug("disconnect is work")
 microgear.setalias("Pi")
@@ -33,13 +39,21 @@ microgear.on_disconnect = disconnect
 microgear.subscribe("/mails")
 microgear.connect(False)
 
+# Read data from Json
+with open("plantInfo.json", "r") as read_file:
+    plantAll = json.load(read_file)
+
+
 #MAIN FUNCTION
 count=0
 
+def SearchData(name):
+    data = plantAll[name] 
+    return data 
 
 while True:
-    microgear.chat("PlantyPot_web",str(count))
-    microgear.chat("Plant_Detail",str(count))
-    count=count+1
-    print(count)
+    microgear.chat("PlantyPot_web",str(count))  
+    microgear.chat("Plant_Detail",str(result))
+    #count=count+1
+    #print(count)
     time.sleep(1)
