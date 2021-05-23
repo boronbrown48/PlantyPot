1a46d488efc520c33f387ee11d05cf2b029b26ff
boronbrown48 <parinee.sr@gmail.com>
2021-05-23 12:21:38 +0700

    no edit

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import requests ,json
 import microgear.client as microgear
 import time
 import logging
-
+import random
 #Global variable
 result = json.dumps(None)
 
@@ -17,7 +17,6 @@ msg ="gu sent this message from line notify"
 #response = requests.post(url, headers=headers, data = {'message':msg}) #command for sending message to line
 
 
-
 #connect with NETPIE
 appid = "PlantyPot"
 gearkey = "PFq6FAxQcaBr6ec"
@@ -25,13 +24,16 @@ gearsecret =  "GwFmcGEEQfAmAXXexCbmIUCCI"
 microgear.create(gearkey,gearsecret,appid,{'debugmode': True})
 def connection():
     logging.info("Now I am connected with netpie")
+
 def subscription(topic,message):
     logging.info(topic+" "+message)
     global result
     msg = message.split("'")
     result = SearchData(msg[1])
+
 def disconnect():
     logging.debug("disconnect is work")
+
 microgear.setalias("Pi")
 microgear.on_connect = connection
 microgear.on_message = subscription
@@ -44,14 +46,20 @@ microgear.connect(False)
 with open("plantInfo.json", "r") as read_file:
     plantAll = json.load(read_file)
 
-
-
 #MAIN FUNCTION
 count=0
 
 while True:
-    microgear.chat("PlantyPot_web",str(count))  
-    microgear.chat("Plant_Detail",str(result))
+
+    #microgear.chat("PlantyPot_web",str(count))  
+    valueObject = {
+        "thaiName" : "thai",
+        "plantCurrentMoist" : random.randint(0,100),
+        "lastestWatering" : "13:00 13/05/21",
+        "plantCurrentLight" : random.randint(0,100),
+        "plantEnergy" : random.randint(0,100),
+    }
+    microgear.chat("Plant_Detail",str(valueObject))
     #count=count+1
     #print(count)
     time.sleep(1)
